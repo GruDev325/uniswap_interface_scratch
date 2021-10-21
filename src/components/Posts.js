ceef7ec4643ad5f42bdf29323f3c60685b77b583
GruDev325 <seniordev1225@gmail.com>
2021-10-21 22:19:35 +0300

    add actions, change to application level state from component level

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,18 +1,6 @@
 import React, { Component } from "react";
 
-export default class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-    };
-  }
-  componentWillMount() {
-    // console.log(123)
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => this.setState({ posts: data }));
-  }
+export default class Posts extends Component {  
   render() {
     const postItems = this.state.posts.map((post) => (
       <div key={post.id}>
