8764bd265c75977054f4175ba70dacc449fdbe41
GruDev325 <seniordev1225@gmail.com>
2021-10-21 23:02:09 +0300

    fetch post, fix reducer

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types'
+import { connect } from "react-redux";
 
-export default class Posts extends Component {  
+import { fetchPosts } from "../actions/postActions";
+
+class Posts extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
   render() {
-    const postItems = this.state.posts.map((post) => (
+    const postItems = this.props.posts.map((post) => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -11,8 +18,13 @@ export default class Posts extends Component {
     return (
       <div>
         <h1>Posts</h1>
-        { postItems }
+        {postItems}
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  posts: state.posts.items
+});
+export default connect(mapStateToProps, { fetchPosts })(Posts);
